3a9b4df6af18bad0a10705178ca1eb02f7385418
Caio <caiog.silva2001@gmail.com>
2024-11-14 03:45:54 -0300

    fix: Account and Card classes precision and scale properties fixed

--- src/main/java/com/example/domain/model/Account.java
+++ src/main/java/com/example/domain/model/Account.java
@@ -17,10 +17,10 @@ public class Account {
     private String agency;
 
     // Definida como valores não nulos e de precisão de 11 casas inteiras e 2 decimais
-    @Column(nullable = false, scale = 13, precision = 2)
+    @Column(nullable = false, precision = 13, scale = 2)
     private BigDecimal balance;
 
-    @Column(name = "additional_limit", nullable = false, scale = 13, precision = 2)
+    @Column(name = "additional_limit", nullable = false, precision = 13, scale = 2)
     private BigDecimal limit;
 
     public Long getId() {
